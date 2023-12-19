95a3129ab22262c06382c0ec2267976ac9f8d98a
ensky0 <ensky0@gmail.com>
2023-12-19 18:41:55 +0900

    update latest delete deprecated distutils expose distances.dist function

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-from distutils.command.build_ext import build_ext
-from distutils.core import Distribution, Extension
+from Cython.Distutils.build_ext import build_ext
+from setuptools import Distribution, Extension
 
 from Cython.Build import cythonize
 
@@ -35,7 +35,6 @@ def build():
     cmd.ensure_finalized()
     cmd.run()
 
-    # Copy built extensions back to the project
     for output in cmd.get_outputs():
         relative_extension = os.path.relpath(output, cmd.build_lib)
         shutil.copyfile(output, relative_extension)
